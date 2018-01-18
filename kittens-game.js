e2a2a0663661522bbfafb732801e482fa847c58e
Paul Dockery <paul.david.dockery@gmail.com>
2018-01-18 12:41:30 -0800

    add craft automation attempt pre-testing, observatory built

--- kittens-game.js
+++ kittens-game.js
@@ -1,8 +1,3 @@
-//requires JQuery on page
-function getBtnWSpan(btnSpan) {
-  $('.btn .btnContent span')
-}
-
 
 function autoCatnip(ms = 5) {
   // ms = ms || 5;
@@ -20,19 +15,20 @@ function autoBuild(buildPriorities) {
   buildPriorities = buildPriorities || [
     'Unic. Pasture',
     'Amphitheatre',
-    'Catnip field',
-    'Pasture',
+    'Temple',
     'Aqueduct',
+    'Catnip field',
     'Pasture',
     'Lumber Mill',
     'Mine',
-    'Temple',
+    'Observatory',
     'Academy',
     'Library',
     'Barn',
     'Warehouse',
     'Tradepost',
-    'Workshop'
+    'Workshop',
+    'Ziggurat'
   ];
 
   let $clickableBtns = $('.bldGroupContainer .btn').not('.disabled');
@@ -54,8 +50,70 @@ function autoBuild(buildPriorities) {
 }
 
 function autoCraft() {
-  let $res = $('#resContainer .resourceRow');
-  let $crafts = $('#craftContainer .resourceRow');
+  if ($('.tabsContainer .activeTab')[0].innerText !== 'Bonfire') {
+    return;
+  }
+
+  let $resCaps = $('#resContainer .resourceRow'),
+      capped = [];
+      $craftables = $('#craftContainer .resourceRow'),
+      craftClickMap,
+      $singleResParams,
+      allResParams = {};
+
+  const resCraftKey = {
+    catnip: 'wood',
+    wood: 'beam',
+    minerals: 'slab',
+    coal: 'steel',
+    iron: 'plate',
+    gold: 'TBD',
+    catpower: 'TBD',
+    science: 'TBD',
+    culture: 'TBD',
+    faith: 'TBD'
+  };
+
+  $resCaps.forEach(row => {
+    let $max = $('.maxRes', row)[0];
+    if ($max) {
+      if ($('.resAmount', row)[0].innerText === $max.innerText) {
+        capped.push($('.resource-name', row)[0].innerText);
+      }
+    }
+    // $('.maxRes', $res[0])[0].innerText
+    // $('.resAmount', $res[0])[0].innerText
+  });
+  console.log('capped', capped);
+
+  capped = capped
+    .map(resource => resCraftKey[resource])
+    .filter(craftable => craftable !== 'TBD');
+  console.log('capped post-filter', capped);
+
+  craftClickMap = $.map($craftables, (craftable, idx) => {
+    return $('.resource-name', craftable)[0].innerText;
+  });
+  console.log('craftClickMap', craftClickMap);
+
+  capped.forEach(resource => {
+    let idx = craftClickMap.indexOf(resource);
+    console.log(resource + ' crafted');
+    $craftables[idx].click();
+  });
+  // $resCaps.forEach(row => {
+  //   let $singleParams = $('td', row),
+  //       singleParams = {};
+
+  //   $singleParams.forEach(elem => {
+  //     singleParams[elem.className] = elem.innerText;
+  //   });
+
+  //   allResParams[singleParams['resource-name']] = singleParams;
+  // });
+  // console.log('allResParams', allResParams);
+
+  setTimeout(() => { autoCraft(); }, 5000);  
 };
 
 function autoAll() {
