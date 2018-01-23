506690408d24fb2abf13523f31d6c658b560b6c4
Paul Dockery <paul.david.dockery@gmail.com>
2018-01-22 18:22:55 -0800

    MAJOR refactoring of autoCraft - maintains ratios
    -code more DRY, includes getAmount(), craft(), and testRatios()
    -reformat craft recipes object to pick/choose considered materials
    -comment code for ease of reference

--- kittens-game.js
+++ kittens-game.js
@@ -1,14 +1,16 @@
 //For use with Kittens Game:  http://bloodrizer.ru/games/kittens/#
 
-// const style = {
-//   craft: 'color: blue',
-//   build: 'color: green; font-weight: bold',
-//   event: ''
-// };
+const style = {
+  craft: 'color: blue',
+  build: 'color: green; font-weight: bold',
+  event: ''
+};
+
 
 function getActiveTab() {
   return $('.tabsContainer .activeTab')[0].innerText;
 }
+
 
 function autoCatnip(ms = 20) {
   if (getActiveTab() === 'Bonfire') {
@@ -16,6 +18,7 @@ function autoCatnip(ms = 20) {
     setTimeout(autoCatnip, ms, ms);
   }
 }
+
 
 function autoBuild() {
   if (getActiveTab() !== 'Bonfire') { return; }
@@ -36,10 +39,10 @@ function autoBuild() {
     'Warehouse', //beam, slab
     'Harbour', //scaffold, slab, plate
     'Mine', //minerals, plate, gold
-    'Quarry', //scaffold, steel, slab
+    // 'Quarry', //scaffold, steel, slab
     'Lumber Mill', //wood, minerals, iron
-    'Oil Well', //steel, gear, scaffold
-    'Steamworks', //steel, gear, blueprint
+    // 'Oil Well', //steel, gear, scaffold
+    // 'Steamworks', //steel, gear, blueprint
     // 'Magneto', //alloy, gear, blueprint
     'Smelter', //minerals
     // 'Calciner', //steel, titanium, blueprint, oil
@@ -93,78 +96,72 @@ function autoCraft() {
     'coal': 'steel',
     'iron': 'plate',
     'titanium': 'alloy',
-//     'gold': 'TBD',
-//     'oil': 'TBD',
-//     'catpower': 'TBD', //auto-hunts special case
+    // 'gold': 'TBD',
+    // 'oil': 'TBD',
+    // 'catpower': 'TBD', //auto-hunts special case
     'science': 'compendium', //risky
-    'culture': 'manuscript', //risky
-//     'faith': 'TBD', //auto-praise special case
-//     'kittens': 'TBD',
+    // 'culture': 'manuscript', //risky
+    // 'faith': 'TBD', //auto-praise special case
+    // 'kittens': 'TBD',
   };
 
-  //determines which uncapped, craftable resources check for autoCraft
-  const craftableList = {
+  const ratioModifier = 4.05; //probably adjust to fit workshop bonus
 
-  };
-
-  const ratioModifier = 3.94; //probably adjust to fit workshop bonus
-
+  //adjust to activate/deactive resources to consider for autoCrafting
+  //also can adjust which materials are considered
+  // TODO: write function to handle mats not in #craftContainer
   const craftRecipes = {
-    wood: {
-      catnip: 50
-    },
-    beam: {
-      wood: 175
-    },
-    slab: {
-      minerals: 250
-    },
-    plate: {
-      iron: 125
-    },
-    steel: {
-      iron: 100,
-      coal: 100
-    },
-    concrete: {
-      slab: 2500,
-      steel: 25
-    },
-    gear: {
-      steel: 15
-    },
-    alloy: {
-      steel: 75,
-      titanium: 10
-    },
-    scaffold: {
-      beam: 50
-    },
-    ship: {
-      scaffold: 100,
-      plate: 150,
-      starchart: 25
-    },
-    parchment: {
-      furs: 175
-    },
-    manuscript: {
-      parchment: 125,
-      culture: 400
-    },
-    compendium: {
-      manuscript: 50,
-      science: 10000
-    },
-    blueprint: {
-      compendium: 25,
-      science: 25000
-    },
-    megalith: {
-      slab: 50,
-      beam: 25,
-      plate: 5
+    // wood: { catnip: 50 },
+    // beam: { wood: 175 },
+    // slab: { minerals: 250 },
+    // plate: { iron: 125 },
+    // steel: { iron: 100, coal: 100 },
+    // concrete: { slab: 2500, steel: 25 },
+    // gear: { steel: 15 },
+    // alloy: { steel: 75, titanium: 10 },
+    scaffold: { beam: 50 },
+    ship: { scaffold: 100, plate: 150,
+            // starchart: 25
+          },
+    // parchment: { furs: 175 },
+    manuscript: { parchment: 125,
+                  // culture: 400
+                },
+    // compendium: { manuscript: 50,
+    //               // science: 10000
+    //             },
+    blueprint: { compendium: 25,
+                 // science: 25000
+               },
+    megalith: { slab: 50, beam: 25, plate: 5 }
+  };
+
+
+  function getAmount(resource) {
+    let $craftableRow = craftablesKey[resource];
+    let amountStr = $('td', $craftableRow)[1].innerText;
+    return convertQuant(amountStr);
+  }
+
+
+  function craft(resource, convertCol) {
+    //convertCol from 1-4, not 0-3
+    let $resourceRow = craftablesKey[resource],
+        $craftBtn = $('td a', $resourceRow)[convertCol - 1];
+    
+    if ($craftBtn.style.display !== 'none') {
+      $craftBtn.click();
+      // console.log('%cCrafted: ' + resource, style.craft);
     }
+  }
+
+
+  function testRatios(resource) {
+    let ratios = craftRecipes[resource];
+    for (let component in ratios) {
+      if (getAmount(component) * ratioModifier < getAmount(resource) * ratios[component]) { return false; }
+    }
+    return true;
   }
 
 
@@ -179,35 +176,33 @@ function autoCraft() {
     return name.substr(0, name.length - 1);
   });
 
-
+  //build reference for DOM element by resource name
   $.each($craftables, (idx, craftable) => {
     let name = $('td', craftable)[0].innerText;
     name = name.substr(0, name.length - 1);
     craftablesKey[name] = craftable;
   });
 
+  //Build array of capped resources
   $.each($resCaps, (idx, elem) => {
     let max = $('.maxRes', elem)[0].innerText,
         current = convertQuant($('.resAmount', elem)[0].innerText),
         name = $('.resource-name', elem)[0].innerText;
+    
     max = convertQuant(max.substring(1, max.length));
     name = name.substr(0, name.length - 1);
     
-    if (max && max !== 0 && max !== '') {
-      if (current >= max) {
-        if (name === 'catpower') {
-          $('#fastHuntContainer a')[0].click();
-          console.log('%cHunted', quickEventStyle);
-          let parchmentLoc = craftClickMap.indexOf('parchment'),
-              $parchmentAll = $('td a', $craftables[parchmentLoc])[3];
-          setTimeout(() => { $parchmentAll.click(); }, 1000);
-        } else if (name === 'faith') {
-          $('#fastPraiseContainer a').click();
-          console.log('Praised the sun', quickEventStyle);
-        } else {
-          capped.push(name);
-        }
-      }
+    if (max && max !== 0 && max !== '' && current >= max) {
+      if (name === 'catpower') {
+        $('#fastHuntContainer a')[0].click();
+        console.log('%cHunted', style.event);
+        setTimeout(craft, 1000, 'parchment', 4);
+      
+      } else if (name === 'faith') {
+        $('#fastPraiseContainer a').click();
+        console.log('%cPraised the sun', quickEventStyle);
+      
+      } else { capped.push(name); }
     }
   });
 
@@ -215,35 +210,11 @@ function autoCraft() {
     .map(resource => cappedResourceKey[resource])
     .filter(craftable => craftable && craftable !== 'TBD');
 
-  function getAmount(resource) {
-    return convertQuant(craftablesKey[resource][1]);
+  capped.forEach(resource => craft(resource, 1));
+
+  for (let craftable in craftRecipes) {
+    if (testRatios(craftable)) { craft(craftable, 1); }
   }
-
-  function convert(resource, convertCol) {
-    //convertCol from 1-4, not 0-3
-    let $resourceRow = craftablesKey[resource];
-    $('td a', $resourceRow)[convertCol - 1].click();
-  }
-
-    //TODO: Make this a function, DRY
-  capped.forEach(resource => {
-    let idx = craftClickMap.indexOf(resource),
-        $addBtn = $('td a', $craftables[idx])[0];
-
-    if ($addBtn.style.display !== 'none') {
-      $addBtn.click();
-      if (resource === 'beam') {
-        let idx2 = craftClickMap.indexOf('scaffold');
-        let $addBtn2 = $('td a', $craftables[idx2])[0];
-        if ($addBtn2.style.display !== 'none') {
-          $addBtn2.click();
-          console.log('%cCrafted: scaffold', style.craft);
-        }
-      } else {
-        console.log('%cCrafted: ' + resource, style.craft);
-      }
-    }
-  });
 
   setTimeout(autoCraft, 1000);  
 };
